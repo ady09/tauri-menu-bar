a735f1e4e0c450e2750bc642310fceb713bcb56b
ady09 <adityayadav8711@gmail.com>
2024-07-19 03:19:36 +0530

    add menubar options

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,47 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
+fn create_app_menu() -> Menu {
+  let recent_menu = Menu:: new()
+  .add_item(CustomMenuItem::new("recent1".to_string(), "Recent 1"))
+  .add_item(CustomMenuItem::new("recent2".to_string(), "Recent 2"));
+
+  return Menu::new()
+      .add_submenu(Submenu::new(
+          "App",
+          Menu::new().add_native_item(MenuItem::Quit),
+      ))
+      .add_submenu(Submenu::new(
+          "File",
+          Menu::new().add_submenu(Submenu::new(
+              "Open Recent",
+              recent_menu,
+          ))
+      ));
+}
 
 fn main() {
   tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+      .menu(create_app_menu())
+      .on_menu_event(|event| {
+          let window = event.window();
+          match event.menu_item_id() {
+              "recent1" => {
+                  let item = "Recent 1".to_string();
+                  window.emit("item-clicked", item).expect("Something Went Wrong!");
+              }
+              "recent2" => {
+                  let item = "Recent 2".to_string();
+                  window.emit("item-clicked", item).expect("Something Went Wrong!");
+              }
+              _ => {}
+          }
+      })
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }
